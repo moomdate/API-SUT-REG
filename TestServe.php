49deba54969c60f235cc7702a7b1c898cc8ea0b7
Surasak sincharoen <moomdate@gmail.com>
2017-10-11 18:26:05 +0700

    edit test serve

--- TestServe.php
+++ TestServe.php
@@ -1,4 +1,16 @@
 <?php
+$url = ["reg.sut.ac.th","reg2.sut.ac.th","reg3.sut.ac.th","reg4.sut.ac.th","reg5.sut.ac.th","google.com"];
+foreach ($url  as $key => $value) {
+	$ch = curl_init($value); //get url http://www.xxxx.com/cru.php?url=http://www.example.com
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	if(curl_exec($ch))
+	{
+		$info = curl_getinfo($ch);
+		echo 'Took ' . $info['total_time'] . ' seconds to transfer a request to ' . $info['url'].'<br>';
+	}
+	
+	curl_close($ch);
+}
 
-echo file_get_contents('http://reg.sut.ac.th');
+
 ?>
